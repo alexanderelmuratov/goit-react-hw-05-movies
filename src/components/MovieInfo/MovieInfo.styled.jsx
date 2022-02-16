12cd291533eea8856fe92ec643f24d27205cc961
alexanderelmuratov <elmuratov@gmail.com>
2022-02-17 00:28:20 +0200

    Update MovieInfo.styled.jsx

--- src/components/MovieInfo/MovieInfo.styled.jsx
+++ src/components/MovieInfo/MovieInfo.styled.jsx
@@ -1,13 +1,24 @@
 import styled from "styled-components";
 
-export const MovieCard = styled.div`
+export const MovieCard = styled.div`  
   display: flex;
+  flex-direction: column;
   margin-bottom: 15px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const MovieImage = styled.img`
-  width: 200px;
-  margin-right: 20px;
+  width: 100%;
+  margin-bottom: 15px;
+
+  @media (min-width: 768px) {
+    width: 200px;
+    margin-right: 20px;
+    margin-bottom: 0;
+  }
 `;
 
 export const MovieText = styled.p`
